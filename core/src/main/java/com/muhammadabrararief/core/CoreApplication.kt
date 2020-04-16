7ca896d82e1716e3f8b5ffd9e9bba89de28a7da7
ariefabrar <abrar.arief@gmail.com>
2020-04-16 12:41:02 +0700

    init DI in application class

--- core/src/main/java/com/muhammadabrararief/core/CoreApplication.kt
+++ core/src/main/java/com/muhammadabrararief/core/CoreApplication.kt
@@ -1,11 +1,22 @@
 package com.muhammadabrararief.core
 
 import android.app.Application
+import com.muhammadabrararief.core.di.CoreComponent
+import com.muhammadabrararief.core.di.CoreModule
+import com.muhammadabrararief.core.di.DaggerCoreComponent
 
 open class CoreApplication : Application() {
 
-    override fun onCreate() {
-        super.onCreate()
+    companion object {
+        lateinit var coreComponent: CoreComponent
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        initDI()
+    }
+
+    private fun initDI() {
+        coreComponent = DaggerCoreComponent.builder().coreModule(CoreModule(this)).build()
+    }
 }
