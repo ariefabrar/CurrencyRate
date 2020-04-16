bb5646ddbe484ef1d2407774e629de2c3fe6a91c
ariefabrar <abrar.arief@gmail.com>
2020-04-16 18:59:04 +0700

    remove comment and unused import

--- converter/src/main/java/com/muhammadabrararief/currencyrate/list/ListActivity.kt
+++ converter/src/main/java/com/muhammadabrararief/currencyrate/list/ListActivity.kt
@@ -2,16 +2,12 @@ package com.muhammadabrararief.currencyrate.list
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.muhammadabrararief.core.base.CoreActivity
-import com.muhammadabrararief.core.network.Outcome
 import com.muhammadabrararief.currencyrate.R
 import com.muhammadabrararief.currencyrate.common.ConverterDH
 import kotlinx.android.synthetic.main.activity_list.*
-import java.io.IOException
 import javax.inject.Inject
 
 class ListActivity : CoreActivity(), RatesAdapter.Listener {
@@ -41,33 +37,6 @@ class ListActivity : CoreActivity(), RatesAdapter.Listener {
     }
 
     private fun initDataObserver() {
-        //Observe the outcome and update state of the screen accordingly
-//        viewModel.ratesOutcome.observe(this, Observer<Outcome<List<Rate>>> { outcome ->
-//            when (outcome) {
-//
-//                is Outcome.Success -> {
-//                    adapter.submitList(outcome.data.toMutableList())
-//                }
-//
-//                is Outcome.Failure -> {
-//                    outcome.e.printStackTrace()
-//                    if (outcome.e is IOException)
-//                        Toast.makeText(
-//                            context,
-//                            "Need Internet to fetch latest rates!",
-//                            Toast.LENGTH_LONG
-//                        ).show()
-//                    else
-//                        Toast.makeText(
-//                            context,
-//                            "Failed to load posts. Please try again later.",
-//                            Toast.LENGTH_LONG
-//                        ).show()
-//                }
-//
-//            }
-//        })
-
         viewModel.rates.observe(this, Observer { t -> adapter.submitList(t) })
     }
 
